3c867e2ad215c0a72f4fffb53adf61d373634d00
Marco Biedermann <marco.biedermann@gmx.de>
2016-03-08 10:14:40 +0100

    improve javascript

--- src/superplaceholder.js
+++ src/superplaceholder.js
@@ -1,13 +1,16 @@
-;(function () {
+;(function (window, document) {
 	var test = document.createElement('input');
-    var isPlaceHolderSupported = ('placeholder' in test);
+	var isPlaceHolderSupported = ('placeholder' in test);
 
 	// Helpers
 	function extend(obj1, obj2) {
 		var obj = {};
-		for (var key in obj1) {
+		var key;
+
+		for (key in obj1) {
 			obj[key] = obj2[key] === undefined ? obj1[key] : obj2[key];
 		}
+
 		return obj;
 	}
 
@@ -32,12 +35,15 @@
 	}
 
 	PlaceHolder.prototype.begin = function() {
-		var self = this,
-			temp,
-			randomIndex;
+		var self = this;
+		var temp;
+		var randomIndex;
+		var i;
+
 		self.originalPlaceholder = self.el.getAttribute('placeholder');
+
 		if (self.options.shuffle) {
-			for (var i = self.texts.length; i--;) {
+			for (i = self.texts.length; i--;) {
 				randomIndex = ~~(Math.random() * i);
 				temp = self.texts[randomIndex];
 				self.texts[randomIndex] = self.texts[i];
@@ -52,43 +58,50 @@
 			self.el.addEventListener('blur', function () {
 				self.cleanUp();
 			});
-		}
-		else {
+		} else {
 			self.processText(0);
 		}
 	};
 
 	PlaceHolder.prototype.cleanUp = function () {
 		// Stop timeouts
-		for (var i = this.timeouts.length; i--;) {
+		var i;
+
+		for (i = this.timeouts.length; i--;) {
 			clearTimeout(this.timeouts[i]);
 		}
+
 		this.el.setAttribute('placeholder', this.originalPlaceholder);
 		this.timeouts.length = 0;
 	};
 
 	PlaceHolder.prototype.typeString = function (str, callback) {
-		var self = this,
-			timeout;
+		var self = this;
+		var timeout;
+		var i;
+		var length;
 
 		if (!str) { return false; }
+
 		function setTimeoutCallback(index) {
 			// Add cursor `|` after current substring unless we are showing last
 			// character of the string.
 			self.el.setAttribute('placeholder', str.substr(0, index + 1) + (index === str.length - 1 || !self.options.showCursor ? '' : self.options.cursor));
+
 			if (index === str.length - 1) {
 				callback();
 			}
 		}
-		for (var i = 0; i < str.length; i++) {
+
+		for (i = 0, length = str.length; i < length; i++) {
 			timeout = setTimeout(setTimeoutCallback, i * self.options.letterDelay, i);
 			self.timeouts.push(timeout);
 		}
 	};
 
 	PlaceHolder.prototype.processText = function(index) {
-		var self = this,
-			timeout;
+		var self = this;
+		var timeout;
 
 		self.typeString(self.texts[index], function () {
 			timeout = setTimeout(function () {
@@ -99,17 +112,20 @@
 	};
 
 	var superplaceholder = function (params) {
-		if (!isPlaceHolderSupported) { return; }
+		if (!isPlaceHolderSupported) {
+			return;
+		}
+
 		new PlaceHolder(params.el, params.sentences, params.options);
 	};
 
 	// open to the world.
 	// commonjs
-	if( typeof exports === 'object' )  {
+	if (typeof exports === 'object')  {
 		module.exports = superplaceholder;
 	}
 	// AMD module
-	else if( typeof define === 'function' && define.amd ) {
+	else if (typeof define === 'function' && define.amd) {
 		define(function () {
 			return superplaceholder;
 		});
@@ -118,4 +134,4 @@
 	else {
 		window.superplaceholder = superplaceholder;
 	}
-})();
+})(window, document);
